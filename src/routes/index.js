59f6f2f7842446cdf293840cb20c22d252d81c98
FerYotta <feryotta2@gmail.com>
2019-10-03 15:06:04 -0400

    Creación de guardado archivo books.json y modificacion de listado y guardado de index,ejes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 const { Router } = require("express");
 const router = Router();
+const fs = require("fs"); // Guardar archivo
+
+// read file
+const json_books = fs.readFileSync("src/books.json", "utf-8");
+const books = JSON.parse(json_books);
 
 router.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {
+    books
+  });
 });
 
 router.get("/new-entry", (req, res) => {
@@ -10,7 +17,26 @@ router.get("/new-entry", (req, res) => {
 });
 
 router.post("/new-entry", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
+  const { title, author, image, description } = req.body;
+  if (!title || !author || !description) {
+    res.status(400).send("Entries must have a title adn description");
+    return;
+  }
+  let newBook = {
+    title,
+    author,
+    image,
+    description
+  };
+
+  books.push(newBook);
+
+  const json_books = JSON.stringify(books);
+
+  // write file
+  fs.writeFileSync("src/books.json", json_books, "utf-8");
+
   res.send("received");
 });
 
